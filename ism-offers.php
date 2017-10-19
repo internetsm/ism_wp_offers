ebc3d7b18e409f8d54a77cd0952502990db47cdd
Michele Capicchioni <capimichi@gmail.com>
2017-10-19 09:55:05 +0200

    Inclusi script e css

--- ism-offers.php
+++ ism-offers.php
@@ -9,6 +9,22 @@ Author URI: #
 License: MIT
 */
 
+// https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
+
+add_action('admin_enqueue_scripts', 'ism_offers_admin_scripts');
+
+function ism_offers_admin_scripts()
+{
+//    wp_enqueue_script('ism_jquery', "https://code.jquery.com/jquery-3.2.1.min.js");
+//    wp_enqueue_script('ism_jqueryui', "https://code.jquery.com/ui/jquery-ui-git.js");
+    wp_enqueue_script('ism_jqueryui', plugin_dir_url(__FILE__) . "assets/vendor/jquery-ui/jquery-ui.min.js");
+    wp_enqueue_style('ism_jqueryui_theme', plugin_dir_url(__FILE__) . "/assets/vendor/jquery-ui/themes/flick/theme.css");
+    wp_enqueue_style('ism_jqueryui_css', plugin_dir_url(__FILE__) . "/assets/vendor/jquery-ui/themes/flick/jquery-ui.min.css");
+
+    wp_enqueue_script('ism_app', plugin_dir_url(__FILE__) . "/assets/js/ism_app.js");
+}
+
+
 add_action('init', 'ism_offers_custom_post_type_declaration');
 
 /**
@@ -65,7 +81,23 @@ function ism_offers_metabox()
     add_meta_box('ism_offers_metabox', 'Offerte', 'ism_offers_metabox_template', 'ism_offers', 'side', 'default');
 }
 
+/**
+ *
+ */
 function ism_offers_metabox_template()
 {
-    echo "SI";
+
+    echo ism_get_template("metabox/metabox.php", [
+        "pippo" => "Ciao",
+    ]);
+}
+
+if (!function_exists("ism_get_template")) {
+    function ism_get_template($slug, $args)
+    {
+        ob_start();
+        extract($args);
+        include __DIR__ . "/templates/{$slug}";
+        return ob_get_clean();
+    }
 }
