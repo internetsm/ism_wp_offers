d3963f4d727107c93c5a3cf2c0f7d02009232005
Michele Capicchioni <capimichi@gmail.com>
2018-05-15 09:50:42 +0200

    Fix step type

--- templates/metabox/metabox.php
+++ templates/metabox/metabox.php
@@ -3,7 +3,7 @@
         <label><strong>Prezzo</strong></label>
     </div>
     <div>
-        <input value="<?php echo $ism_offer_data['price']; ?>" type="number" name="ism_offers_price"/>
+        <input value="<?php echo $ism_offer_data['price']; ?>" type="number" step="any" name="ism_offers_price"/>
     </div>
 </div>
 
@@ -15,11 +15,11 @@
     </div>
     <div>
         <label>
-            <input <?php echo (($ism_offer_data['price_type'] == "price_night" || $ism_offer_data['price_type'] == "") ? "checked='checked'" : ""); ?>
-                value="price_night" type="radio" name="ism_offers_price_type"/> Prezzo a
+            <input <?php echo(($ism_offer_data['price_type'] == "price_night" || $ism_offer_data['price_type'] == "") ? "checked='checked'" : ""); ?>
+                    value="price_night" type="radio" name="ism_offers_price_type"/> Prezzo a
             notte</label>
-        <label><input <?php echo ($ism_offer_data['price_type'] == "price_flat" ? "checked='checked'" : ""); ?>
-                value="price_flat" type="radio" name="ism_offers_price_type"/> Prezzo forfettario</label>
+        <label><input <?php echo($ism_offer_data['price_type'] == "price_flat" ? "checked='checked'" : ""); ?>
+                    value="price_flat" type="radio" name="ism_offers_price_type"/> Prezzo forfettario</label>
     </div>
 </div>
 <br/>
@@ -29,16 +29,16 @@
     </div>
     <div>
         <select name="ism_offers_treatment">
-            <option value="all_inclusive" <?php echo ($ism_offer_data['treatment'] == "all_inclusive" ? "selected='selected'" : ""); ?>>
+            <option value="all_inclusive" <?php echo($ism_offer_data['treatment'] == "all_inclusive" ? "selected='selected'" : ""); ?>>
                 All Inclusive
             </option>
-            <option value="fullboard" <?php echo ($ism_offer_data['treatment'] == "fullboard" ? "selected='selected'" : ""); ?>>
+            <option value="fullboard" <?php echo($ism_offer_data['treatment'] == "fullboard" ? "selected='selected'" : ""); ?>>
                 Pensione completa
             </option>
-            <option value="halfboard" <?php echo ($ism_offer_data['treatment'] == "halfboard" ? "selected='selected'" : ""); ?>>
+            <option value="halfboard" <?php echo($ism_offer_data['treatment'] == "halfboard" ? "selected='selected'" : ""); ?>>
                 Mezza pensione
             </option>
-            <option value="bed_and_breakfast" <?php echo ($ism_offer_data['treatment'] == "bed_and_breakfast" ? "selected='selected'" : ""); ?>>
+            <option value="bed_and_breakfast" <?php echo($ism_offer_data['treatment'] == "bed_and_breakfast" ? "selected='selected'" : ""); ?>>
                 Bed and breakfast
             </option>
         </select>
@@ -50,7 +50,8 @@
         <label><strong>Data arrivo</strong></label>
     </div>
     <div>
-        <input required="required" value="<?php echo $ism_offer_data['date_arrival']; ?>" class="datepicker" id="datepicker-arrival"
+        <input required="required" value="<?php echo $ism_offer_data['date_arrival']; ?>" class="datepicker"
+               id="datepicker-arrival"
                type="text"
                name="ism_offers_date_arrival"/>
     </div>
@@ -61,7 +62,8 @@
         <label><strong>Data partenza</strong></label>
     </div>
     <div>
-        <input required="required" value="<?php echo $ism_offer_data['date_departure']; ?>" id="datepicker-departure" class="datepicker"
+        <input required="required" value="<?php echo $ism_offer_data['date_departure']; ?>" id="datepicker-departure"
+               class="datepicker"
                type="text" name="ism_offers_date_departure"/>
     </div>
 </div>
