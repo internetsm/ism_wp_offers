30bb682167bbd1b8b961a481a2b779b1706d5888
Michele Capicchioni <capimichi@gmail.com>
2018-07-17 11:26:40 +0200

    Fix category

--- includes/taxonomies-functions.php
+++ includes/taxonomies-functions.php
@@ -8,15 +8,16 @@
 
 add_action("init", "ism_offers_taxonomies");
 
-function ism_offers_taxonomies(){
+function ism_offers_taxonomies()
+{
 
 //    register_taxonomy( 'tipology', 'ism_offer');
     register_taxonomy(
         'ism_offers_category',
         'ism_offer',
         array(
-            'label' => "Categoria",
-            'rewrite' => array( 'slug' => __("category", "ism_offers") ),
+            'label'        => "Categoria",
+            'rewrite'      => array('slug' => "categoria-offerte"),
             'hierarchical' => true,
         )
     );
